4c7c19861a6f8876a8bf7c55be6330fa895979ab
g-ch <947089399@qq.com>
2018-11-25 18:48:59 +0800

    change range and add new worlds

--- src/gazebo_ros_pkgs/gazebo_plugins/src/gazebo_ros_openni_kinect.cpp
+++ src/gazebo_ros_pkgs/gazebo_plugins/src/gazebo_ros_openni_kinect.cpp
@@ -99,7 +99,7 @@ void GazeboRosOpenniKinect::Load(sensors::SensorPtr _parent, sdf::ElementPtr _sd
   else
     this->point_cloud_cutoff_ = _sdf->GetElement("pointCloudCutoff")->Get<double>();
   if (!_sdf->HasElement("pointCloudCutoffMax"))
-    this->point_cloud_cutoff_max_ = 5.0;
+    this->point_cloud_cutoff_max_ = 8.0;  // CHG Ori: 5.0
   else
     this->point_cloud_cutoff_max_ = _sdf->GetElement("pointCloudCutoffMax")->Get<double>();
 
